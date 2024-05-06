ab957761923aa0503718cb707a0f538bfaffe309
Maarcinn <marcinsz4832@gmail.com>
2024-05-06 19:41:52 +0200

    Helloword terraform

--- main.tf
+++ main.tf
@@ -1,12 +1,8 @@
-# main.tf
-
-# Definicja zmiennych
 variable "greeting" {
   type    = string
   default = "Hello, world"
 }
 
-# Zasób pliku, który wyświetli powitanie
 resource "local_file" "hello_world" {
   filename = "${path.module}/hello_world.txt"
   content  = var.greeting
--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,3 @@
-# outputs.tf
-
-# Wyjście zasobu pliku
 output "hello_world_content" {
   value = local_file.hello_world.content
 }
